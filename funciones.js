88b6d34057297c64003297e4143d94de2526d6a7
CanarioEchazu <darioechazu@hotmail.com>
2022-09-12 12:55:26 -0300

    aprendiendo basicos

---
+++ funciones.js
@@ -0,0 +1,29 @@
+//Declarativas
+function miFuncion () {
+    return 3;
+};
+
+miFuncion();
+
+//Expresion
+
+var otraFuncion = function(a,b){
+    return a + b;
+};
+
+otraFuncion();
+
+// var alumnos = ['Daniela','Dario','Ignacio'];
+
+function saludarEstudiantes(estudiante){
+    console.log(`Hola ${estudiante}`)
+};
+saludarEstudiantes("Dario");
+
+function sumar(a,b) {
+    var resultado = (a+b);
+    return resultado;
+}
+console.log (sumar (1,2));
+
+
---
+++ variables.js
@@ -0,0 +1,20 @@
+var nombre = "Dario";
+
+// declarar
+var edad;
+// incializar
+edad = 30;
+
+edad
+
+var elementos = ['PC','Celular'];
+elementos;
+
+var persona = {
+    nombre: "Dario",
+    Edad: 52
+};
+
+persona;
+
+console.log (persona.Edad);
